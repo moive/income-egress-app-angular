c0d510ba7405611c970a92034cc78e9e68f84b94
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-11-24 15:02:46 -0500

    display fullname in header navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filter } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  fullName: string = '';
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select('user')
+      .pipe(filter(({ user }) => user != null))
+      .subscribe(({ user }) => (this.fullName = user?.name!));
   }
-
 }
