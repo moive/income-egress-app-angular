68cefecd6aa26d035500126bd28a523108f93b92
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-11-24 23:57:19 -0500

    canLoad replace canActivate for security page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   {
     path: '',
+    // canActivate: [AuthGuard],
+    canLoad: [AuthGuard],
     loadChildren: () =>
       import('./income-egress/income-egress.module').then(
         (m) => m.IncomeEgressModule
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { Observable, take, tap } from 'rxjs';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): Observable<boolean> {
@@ -20,6 +21,15 @@ export class AuthGuard implements CanActivate {
       tap((state) => {
         if (!state) this.router.navigate(['/login']);
       })
+    );
+  }
+
+  canLoad(): Observable<boolean> {
+    return this.authService.isAuth().pipe(
+      tap((state) => {
+        if (!state) this.router.navigate(['/login']);
+      }),
+      take(1)
     );
   }
 }
